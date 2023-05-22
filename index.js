5a73bce9f948b232a9d39c3e06ae69812259dfbb
sunnyghosh108 <sunnyghoshindra009@gmail.com>
2023-05-22 16:42:51 +0600

    get toys data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express =require('express');
 const cors =require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app =express();
 const port = process.env.PORT || 5000;
@@ -29,6 +29,80 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productsCollecton=client.db('toysMarket').collection('products');
+  
+    app.get('/products',async(req,res)=>{
+      const cursor =productsCollecton.find();
+      const result =await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/products',async(req,res)=>{
+        const newToys = req.body;
+        console.log(newToys);
+        const result =await productsCollecton.insertOne(newToys);
+        res.send(result);
+    })
+    //bookings
+    app.get('/products', async(req,res)=>{
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email){
+        query = {email:req.query.email}
+      }
+
+      const result =await productsCollecton.find().toArray();
+      res.send(result);
+    })
+    // 
+    
+    // bookings routes
+  //   app.get('/products', async (req, res) => {
+  //     const decoded = req.decoded;
+    
+  //     let query = {};
+  //     if (req.query?.email) {
+  //         query = { email: req.query.email }
+  //     }
+  //     const result = await bookingCollection.find(query).toArray();
+  //     res.send(result);
+  // })
+
+  // app.post('/products', async (req, res) => {
+  //     const booking = req.body;
+  //     console.log(booking);
+  //     const result = await bookingCollection.insertOne(booking);
+  //     res.send(result);
+  // });
+  
+
+  app.patch('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const updateDoc = {
+          $set: {
+              status: updatedBooking.status
+          },
+      };
+      const result = await productsCollecton.updateOne(filter, updateDoc);
+      res.send(result);
+  })
+
+  app.delete('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await productsCollecton.deleteOne(query);
+      res.send(result);
+  })
+
+
+  //  
+    
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
